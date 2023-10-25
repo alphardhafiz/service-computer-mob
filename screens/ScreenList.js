eb0f51b35ec183a06fa199f4d553822aa5b3a3a2
kazumo92 <adityayusuf92@gmail.com>
2023-10-25 20:19:26 +0700

    coba test list

--- screens/ScreenList.js
+++ screens/ScreenList.js
@@ -1,7 +1,60 @@
 import React from 'react'
 
-export default function ScreenList() {
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Appbar, List } from "react-native-paper";
+
+export default function ScreenList({ navigation, route }) {
+    const [expanded, setExpanded] = useState(true);
+
+    const handlePress = () => setExpanded(!expanded);
+  
+    const [daftarTerima, setDaftarTerima] = useState([]);
+  
+    useEffect(() => {
+      if (route.params.hp) {
+        fetch(`http://localhost:19006/api/barang/${route.params.hp}/list`)
+          .then((response) => response.json())
+          .then((json) => {
+            setDaftarTerima(json);
+          })
+          .catch((err) => console.log(err));
+      }
+    }, []);
+
+
   return (
-    <div>ScreenHasil</div>
-  )
+    <View style={styles.container}>
+      <Appbar.Header>
+        <Appbar.BackAction
+          onPress={() => {
+            navigation.navigate("/");
+          }}
+        />
+        <Appbar.Content title="Hasil" />
+      </Appbar.Header>
+
+      <List.Section title={`${daftarTerima.length} cucian...`}>
+        {daftarTerima.map((terima) => (
+          <List.Accordion
+            key={terima._id}
+            title={terima.customer.nama}
+            expanded={expanded}
+            onPress={handlePress}
+            left={(props) => <List.Icon {...props} icon="folder" />}
+            right={(props) => <Text>{terima.status}</Text>}>
+            {terima.details.map((item, index) => (
+              <>
+                <List.Item key={index} title={item.nama} />
+              </>
+            ))}
+          </List.Accordion>
+        ))}
+      </List.Section>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+});
